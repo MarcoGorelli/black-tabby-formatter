6298ae8e6d6e6793bf3af464b55af039260feae9
Marco Gorelli <marcogorelli@protonmail.com>
2022-02-06 09:15:19 +0000

    init

---
+++ README.md
@@ -0,0 +1,74 @@
+[![Build Status](https://github.com/MarcoGorelli/absolufy-imports/workflows/tox/badge.svg)](https://github.com/MarcoGorelli/absolufy-imports/actions?workflow=tox)
+[![Coverage](https://codecov.io/gh/MarcoGorelli/absolufy-imports/branch/main/graph/badge.svg)](https://codecov.io/gh/MarcoGorelli/absolufy-imports)
+[![pre-commit.ci status](https://results.pre-commit.ci/badge/github/MarcoGorelli/absolufy-imports/main.svg)](https://results.pre-commit.ci/latest/github/MarcoGorelli/absolufy-imports/main)
+
+absolufy-imports
+================
+
+A tool and pre-commit hook to automatically convert relative imports to absolute.
+
+<p align="center">
+    <a href="#readme">
+        <img alt="demo" src="https://raw.githubusercontent.com/nbQA-dev/nbQA-demo/master/abs-imports.gif">
+    </a>
+</p>
+
+## Installation
+
+```console
+$ pip install absolufy-imports
+```
+
+## Usage as a pre-commit hook (recommended)
+
+See [pre-commit](https://github.com/pre-commit/pre-commit) for instructions
+
+Sample `.pre-commit-config.yaml`:
+
+```yaml
+-   repo: https://github.com/MarcoGorelli/absolufy-imports
+    rev: v0.3.1
+    hooks:
+    -   id: absolufy-imports
+```
+
+## Command-line example
+
+```console
+$ absolufy-imports mypackage/myfile.py
+```
+
+```diff
+- from . import __version__
++ from mypackage import __version__
+```
+
+## Configuration
+
+### Application directories
+
+If your package follows the popular `./src` layout, you can pass your application directories via `--application-directories`, e.g.
+
+```console
+$ absolufy-imports src/mypackage/myfile.py --application-directories src
+```
+
+```diff
+- from . import __version__
++ from mypackage import __version__
+```
+
+Multiple application directories should be colon-separated, e.g. `--application-directories .:src`. This is the same as in [reorder-python-imports](https://github.com/asottile/reorder_python_imports).
+
+### Only use relative imports
+
+Use the `--never` flag, e.g.
+
+```console
+$ absolufy-imports mypackage/myfile.py --never
+```
+
+```diff
+- from mypackage import __version__
++ from . import __version__
+```
---
+++ tests/main_test.py
@@ -0,0 +1,61 @@
+import os
+import subprocess
+
+def test_main(tmpdir):
+    content = 'if  True:\n    pass'
+    file = os.path.join(tmpdir, 'tmpfile.py')
+    with open(file, 'w') as fd:
+        fd.write(content)
+    output = subprocess.run(['python', '-m', 'black_tabby_formatter', file], universal_newlines=True, capture_output=True)
+    with open(file) as fd:
+        newcontent = fd.read()
+
+    assert newcontent == 'if True:\n\tpass\n'
+    assert 'reformatted' in output.stderr
+    assert output.returncode == 1
+
+def test_main_noop(tmpdir):
+    content = 'if True:\n\tpass\n'
+    file = os.path.join(tmpdir, 'tmpfile.py')
+    with open(file, 'w') as fd:
+        fd.write(content)
+    output = subprocess.run(['python', '-m', 'black_tabby_formatter', file], universal_newlines=True, capture_output=True)
+    with open(file) as fd:
+        newcontent = fd.read()
+
+    assert newcontent == content
+    assert 'unchanged' in output.stderr
+    assert output.returncode == 0
+
+def test_main_no_black_change(tmpdir):
+    content = 'if True:\n    pass\n'
+    file = os.path.join(tmpdir, 'tmpfile.py')
+    with open(file, 'w') as fd:
+        fd.write(content)
+    output = subprocess.run(['python', '-m', 'black_tabby_formatter', file], universal_newlines=True, capture_output=True)
+    with open(file) as fd:
+        newcontent = fd.read()
+
+    assert newcontent == 'if True:\n\tpass\n'
+    assert 'reformatted' in output.stderr
+    assert output.returncode == 1
+
+def test_main_multiple_files(tmpdir):
+    content1 = 'if True :\n    pass\n'
+    file1 = os.path.join(tmpdir, 'tmpfile1.py')
+    with open(file1, 'w') as fd:
+        fd.write(content1)
+    content2 = 'if False:\n    pass\n'
+    file2 = os.path.join(tmpdir, 'tmpfile2.py')
+    with open(file2, 'w') as fd:
+        fd.write(content2)
+    output = subprocess.run(['python', '-m', 'black_tabby_formatter', file1, file2], universal_newlines=True, capture_output=True)
+    with open(file1) as fd:
+        newcontent1 = fd.read()
+    with open(file2) as fd:
+        newcontent2 = fd.read()
+
+    assert newcontent1 == 'if True:\n\tpass\n'
+    assert newcontent2== 'if False:\n\tpass\n'
+    assert 'reformatted' in output.stderr
+    assert output.returncode == 1
